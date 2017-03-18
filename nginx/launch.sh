7ebb96204e82adcb45e5900851803bbee0590e29
nikolai <n.ivanov.p@gmail.com>
2017-03-18 20:29:43 +0300

    changed .htpasswd

--- nginx/launch.sh
+++ nginx/launch.sh
@@ -3,7 +3,7 @@
 envsubst < nginx.conf > /etc/nginx/nginx.conf
 envsubst < auth.htpasswd > /etc/nginx/.htpasswd
 
-htpasswd -c -b /etc/nginx/auth.htpasswd $USERNAME $PASSWORD
+htpasswd -c -b /etc/nginx/.htpasswd $USERNAME $PASSWORD
 
 echo basic-auth-pwd
 cat /etc/nginx/auth.htpasswd
