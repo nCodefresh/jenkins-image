8a8ff470bb7900bd159884f3b11e661cbfa34dd2
nikolai <n.ivanov.p@gmail.com>
2017-03-18 19:50:26 +0300

    added docker-compose

---
+++ nginx/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+envsubst < nginx.conf > /etc/nginx/nginx.conf
+envsubst < auth.htpasswd > /etc/nginx/.htpasswd
+
+htpasswd -c -b /etc/nginx/auth.htpasswd $USERNAME $PASSWORD
+
+echo basic-auth-pwd
+cat /etc/nginx/auth.htpasswd
+
+nginx -g "daemon off;"
